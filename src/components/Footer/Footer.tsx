f85abbc662264a0376f8c4c3ea82a3cd3bf7aed7
Daniel Domenech Moreno <71819787+ddm19@users.noreply.github.com>
2025-07-31 23:31:30 +0200

    Add Header and Footer components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Footer: React.FC = () => (
+  <footer className="footer">
+    <div className="container footer__container">
+      <div className="footer__section">
+        <h3 className="footer__title">Aqivap</h3>
+        <p className="footer__text">Productos de vapeo de calidad</p>
+      </div>
+      <div className="footer__section">
+        <h3 className="footer__title">Enlaces</h3>
+        <NavLink to="/" className="footer__link">Inicio</NavLink>
+        <NavLink to="/products" className="footer__link">Productos</NavLink>
+        <NavLink to="/about" className="footer__link">Nosotros</NavLink>
+        <NavLink to="/contact" className="footer__link">Contacto</NavLink>
+      </div>
+      <div className="footer__section">
+        <h3 className="footer__title">Síguenos</h3>
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="footer__link">Facebook</a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="footer__link">Instagram</a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="footer__link">Twitter</a>
+      </div>
+    </div>
+    <div className="footer__bottom">© {new Date().getFullYear()} Aqivap</div>
+  </footer>
+)
+
+export default Footer
